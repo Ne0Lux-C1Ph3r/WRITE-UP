2c46847dba9e4c5646c28779770993b17694c98a
Ne0Lux-C1Ph3r <neolux-cipher@hotmail.fr>
2017-02-14 11:51:21 +0100

    Delete index.php

--- BSidesSF/files/index.php
+++
@@ -1,5 +0,0 @@
-<html><head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8"></head><body><h1>Login successful!</h1>
-<p>Setting cookie: <tt>auth=username=guest&amp;date=2017-02-13T10:16:40+0000&amp;</tt></p>
-<p>Click <a href="http://easyauth-afee0e67.ctf.bsidessf.net/index.php">here</a> to continue!</p>
-</body></html>
